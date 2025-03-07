649636b33855c14b0b53ce66178c5cb1e0a0f410
karantec <karanrana3095@gmail.com>
2025-03-07 16:24:24 +0530

    commit

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -68,12 +68,7 @@ function Header(){
 
 
                 {/* Notification icon */}
-                <button className="btn btn-ghost ml-4  btn-circle" onClick={() => openNotification()}>
-                    <div className="indicator">
-                        <BellIcon className="h-6 w-6"/>
-                        {noOfNotifications > 0 ? <span className="indicator-item badge badge-secondary badge-sm">{noOfNotifications}</span> : null }
-                    </div>
-                </button>
+               
 
 
                 {/* Profile icon, opening menu on click */}
